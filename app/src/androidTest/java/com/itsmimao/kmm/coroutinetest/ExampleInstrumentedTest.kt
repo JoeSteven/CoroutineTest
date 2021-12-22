e026c3d8f947abedc2948f162849b9e6dd5958a1
itsMimao <qiaoxiaoxi621@gmail.com>
2021-12-22 14:40:39 +0800

    init repo

---
+++ app/src/androidTest/java/com/itsmimao/kmm/coroutinetest/ExampleInstrumentedTest.kt
@@ -0,0 +1,37 @@
+package com.itsmimao.kmm.coroutinetest
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.itsmimao.kmm.coroutinetest", appContext.packageName)
+    }
+
+    @Test
+    fun testCoroutine() = runBlocking {
+        assert(testSuspend())
+    }
+
+    private suspend fun testSuspend():Boolean {
+        delay(1000)
+        return true
+    }
+
+}
---
+++ app/src/main/java/com/itsmimao/kmm/coroutinetest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.itsmimao.kmm.coroutinetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
